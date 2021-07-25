ce13e608b217da0730c37809e027a17f0f05570c
Kely Wang <kkwang@andrew.cmu.edu>
2021-07-25 17:17:14 -0400

    fixed align on project pages

--- src/components/ProjectTitle/ProjectTitle.js
+++ src/components/ProjectTitle/ProjectTitle.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 export default function ProjectTitle(props) {
     return (
-        <div className="project-title">
+        <div className="project-title"  style={{paddingLeft: "20px"}}>
             <h1 className="accent small-margin">{ props.title }</h1>
             <h4 className="small-margin">{ props.timeline }</h4>
             <h5 className="small-margin">{ props.category }</h5>
