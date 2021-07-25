46eadf2f78480ab98c699e45f9755b28572de65a
Kely Wang <kkwang@andrew.cmu.edu>
2021-07-25 17:18:44 -0400

    added responsive restyling to ProjectCard

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import "./style.css";
 
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
 export default function ProjectCard(props) {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <a
             href={ props.href }
@@ -12,7 +31,7 @@ export default function ProjectCard(props) {
                 <div className="project-img">
                     <GatsbyImage image={ getImage(props.coverImg) } alt="" />
                 </div>
-                { window.screen.availWidth < 576 ?
+                { windowDimensions.width < 576 ?
                     <div className="project-text">
                         <h2 className="accent">{ props.title }</h2>
                         <h4>{ props.category }</h4>
