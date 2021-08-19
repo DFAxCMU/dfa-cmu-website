38003a3e68e758998c3f5b617d6b5c266b101340
Kely Wang <kkwang@andrew.cmu.edu>
2021-08-18 23:35:27 -0400

    added google calendar

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,7 +6,7 @@ const Schedule = () => {
     <div>
       <TopBar />
       <h2 className="accent">Schedule</h2>
-      <h2 style={{textAlign: "center"}}>Coming Soon!</h2>
+      <iframe src="https://calendar.google.com/calendar/embed?src=carnegiemellon%40designforamerica.com&ctz=America%2FNew_York" title="DFA at CMU Google Calendar" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
       <div style={{marginBottom: "72px"}}></div>
     </div>
   )
