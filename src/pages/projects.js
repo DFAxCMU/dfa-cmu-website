d163de2543f7203997e6adadb81eae4f39a36833
chelstang <86565244+chelstang@users.noreply.github.com>
2022-08-04 20:47:15 -0500

    Merged Archive and Current (one csv file)

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,19 +1,63 @@
 import React from "react";
 import TopBar from "../components/TopBar/TopBar";
+import ProjectCard from "../components/ProjectCard/ProjectCard";
 import { graphql } from "gatsby";
-import ArchivedProjects from "../components/Projects/archived-projects";
-import CurrentProjects from "../components/Projects/current-projects";
 
-const Projects = () => {
+const Projects = ({ data }) => {
+  // formatting for the image data retrieved from GraphQL
+  const imgObject = {};
+  data.covers.edges.forEach(edge => {
+    imgObject[edge.node.relativeDirectory] = edge.node.childImageSharp.gatsbyImageData;
+  });
+
   return (
     <div>
       <TopBar />
       <h2 className="accent">Projects</h2>
-      <ArchivedProjects/>
-      <CurrentProjects/>
+      <div>
+        { data.allArchivedSummaryCsv.edges.map(edge => {
+          const name = edge.node.ID;
+          return (<ProjectCard
+            key={ name }
+            href={ "/" + name }
+            coverImg={ imgObject[name] }
+            title={ edge.node.title }
+            category={ edge.node.category }
+            team={ edge.node.team }
+            body={ edge.node.summary }
+          />);
+        }) }
+      </div>
       <div style={{marginBottom: "72px"}}></div>
     </div>
   )
 };
   
 export default Projects;
+  
+// get information about current projects and their cover images
+export const query = graphql`
+  query ArchivedQuery {
+    allArchivedSummaryCsv {
+      edges {
+        node {
+          title
+          category
+          team
+          summary
+          ID
+        }
+      }
+    }
+    covers: allFile(filter: {name: {eq: "cover"}}) {
+      edges {
+        node {
+          relativeDirectory
+          childImageSharp {
+            gatsbyImageData(height: 200)
+          }
+        }
+      }
+    }
+  }
+`
