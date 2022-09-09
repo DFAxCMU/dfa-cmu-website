a3ddb06e9bd11d680df8489fe39305ac6c8db7d5
chelstang <86565244+chelstang@users.noreply.github.com>
2022-09-09 15:14:29 -0400

    Updated Calendar

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,7 +6,7 @@ const Schedule = () => {
     <div>
       <TopBar />
       <h2 className="accent">Schedule</h2>
-      <iframe src="https://calendar.google.com/calendar/embed?src=carnegiemellon%40designforamerica.com&ctz=America%2FNew_York" title="DFA at CMU Google Calendar" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
+      <iframe src= "https://calendar.google.com/calendar/embed?src=c_jvkv9bu7qkpcnenmsdj8d8mcnc%40group.calendar.google.com&ctz=America%2FNew_York" title="DFA at CMU Google Calendar" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
       <div style={{marginBottom: "72px"}}></div>
     </div>
   )
